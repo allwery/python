28e76807c898f10d1c74354f1ecbd15460a5afbc
allwery <113474834+allwery@users.noreply.github.com>
2024-05-07 15:46:56 +0300

    Create 1.py

---
+++ task5/1.py
@@ -0,0 +1,52 @@
+#1
+def max_r(N, K, heights):
+    pref_sums = [0] * N
+    pref_sums[0] = heights[0]
+    for i in range(1, N):
+        pref_sums[i] = pref_sums[i - 1] + heights[i]
+
+    max_score = -float('inf')
+    best_start = None
+    best_end = None
+
+    for start in range(N - K + 1):
+        end = start + K - 1
+        score = pref_sums[end]
+        if start > 0:
+            score -= pref_sums[start - 1]
+        if score > max_score:
+            max_score = score
+            best_start = start + 1
+            best_end = end + 1
+    return max_score
+
+with open('D:/27-170a.txt', 'r') as f:
+    N, K = map(int, f.readline().split())
+    heights = [int(line) for line in f]
+
+max_a = max_r(N, K, heights)
+
+with open('D:/27-170b.txt', 'r') as f:
+    N, K = map(int, f.readline().split())
+    heights = [int(line) for line in f]
+
+max_b = max_r(N, K, heights)
+print(max_a, max_b)
+
+#2
+import random
+
+with open("f.txt", "w") as f:
+    for i in range(100):
+        f.write(str(random.randint(1, 100)) + "\n")
+
+with open("f.txt", "r") as f, open("g.txt", "w") as g:
+    numbers = set()
+    for line in f:
+        number = int(line.strip())
+        if number not in numbers:
+            g.write(str(number) + "\n")
+            numbers.add(number)
+            
+with open("g.txt", "r") as g:
+    print(g.read())
