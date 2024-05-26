15b2ddc2a45b9eb52123015b5e2fb3afdf968c34
allwery <113474834+allwery@users.noreply.github.com>
2024-05-26 20:43:37 +0300

    Add files via upload

---
+++ Task6/cgi-bin/lawsuit-form.py
@@ -0,0 +1,29 @@
+import cgi
+import sqlite3
+
+con = sqlite3.connect("cgi-bin/software_database.db")
+cursor = con.cursor()
+form = cgi.FieldStorage()
+title = form.getfirst("title")
+version = form.getfirst("version")
+developer = form.getfirst("developer")
+description = form.getfirst("description")
+
+print("Content-type: text/html\n")  # Добавлен заголовок Content-type для правильной работы CGI
+
+print("<html>")
+print()
+print("<head><meta charset=\"UTF-8\"></head>")
+try:
+    cursor.execute('INSERT INTO Software (Title, Version, Developer, Description) VALUES (?, ?, ?, ?)',
+                   (title, version, developer, description))
+    con.commit()
+except Exception as e:
+    print(f"<div>Oshibka pri oformlenii bileta: {e}</div>")
+else:
+    print("<div>Bilet uspeshno dobavlen!</div>")
+finally:
+    con.close()
+    print("<a href=\"../index.html\">Nazad</a>")
+    print()
+    print("</html>")
